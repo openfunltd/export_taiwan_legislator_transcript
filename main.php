6f1396faae275dd14cbf30ae41e851573954ec7d
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-25 13:40:16 +0800

    Download single result of an ivod into /json/single

--- main.php
+++ main.php
@@ -1,7 +1,11 @@
 <?php
 include 'src/initialer.inc.php';
 include 'src/downloader.inc.php';
+//include 'src/parser.inc.php';
 
 Initialer::initalizeProject();
 Downloader::downloadIvods(11);
-Downloader::getIvodList();
+$ivods = Downloader::getIvodList();
+Downloader::downloadIvodsDetail($ivods);
+$detailed_ivods = Downloader::getDetailedIvodList();
+//$csvRows = Parser::parseData($detailed_ivods);
--- src/downloader.inc.php
+++ src/downloader.inc.php
@@ -17,6 +17,51 @@ class Downloader {
         }
     }
 
+    public static function getIvodList() {
+        $content = self::getPaginationIvodList('page1.json', false);
+        $ivods = $content->ivods;
+        for ($page = 2; $page <= $content->total_page; $page++) {
+            $ivods = array_merge($ivods, self::getPaginationIvodList("page$page.json"));
+        }
+        return $ivods;
+    }
+
+    public static function downloadIvodsDetail($ivods) {
+        $ivods = array_filter($ivods, function ($ivod) {
+            return in_array('gazette', $ivod->features);
+        });
+        $total_cnt = count($ivods);
+        $cnt = 0;
+        foreach ($ivods as $ivod) {
+            $cnt++;
+            $ivod_id = $ivod->id;
+            echo "($cnt/$total_cnt) Downloading detailed ivod data... ivod_id: $ivod_id" . "\n";
+            self::downloadSingleIvod($ivod_id);
+        }
+    }
+
+    public static function getDetailedIvodList() {
+        $filenames = scandir('json/single/');
+        $filenames = array_slice($filenames, 2);
+        $detailed_ivods = [];
+        foreach ($filenames as $filename) {
+            $filepath = "json/single/$filename";
+            $content = json_decode(file_get_contents($filepath));
+            $detailed_ivods[] = $content;
+        }
+        return $detailed_ivods;
+    }
+
+    private static function downloadSingleIvod($ivod_id) {
+        $url = "https://ly.govapi.tw/ivod/$ivod_id?with_gazette=1";
+        $ch = curl_init();
+        curl_setopt($ch , CURLOPT_URL , $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $res = curl_exec($ch);
+        curl_close($ch);
+        file_put_contents("json/single/$ivod_id.json", $res);
+    }
+
     private static function paginationDownload($page, $url) {
         $url = $url . "&page=$page";
         $ch = curl_init();
@@ -25,15 +70,6 @@ class Downloader {
         $res = curl_exec($ch);
         curl_close($ch);
         file_put_contents("json/list/page$page.json", $res);
-    }
-
-    public static function getIvodList() {
-        $content = self::getPaginationIvodList('page1.json', false);
-        $ivods = $content->ivods;
-        for ($page = 2; $page <= $content->total_page; $page++) {
-            $ivods = array_merge($ivods, self::getPaginationIvodList("page$page.json"));
-        }
-        return $ivods;
     }
 
     private static function getPaginationIvodList($filename, $justIvods = true) {
