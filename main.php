b0ee865a1de02b74aeb0c9ac5e33c26ba3e6a617
typingmonk <tmonk.hsieh@tmonk.tw>
2024-06-26 08:45:16 +0800

    Add comments

--- main.php
+++ main.php
@@ -5,13 +5,14 @@ include 'src/parser.inc.php';
 
 $term = 11;
 $session_period = 1;
-Initialer::initalizeProject();
-Downloader::downloadIvods($term, $session_period);
-$ivods = Downloader::getIvodList();
-Downloader::downloadIvodsDetail($ivods);
-$detailed_ivods = Downloader::getDetailedIvodList();
+//Initialer::initalizeProject(); //建立 json 資料夾
+//Downloader::downloadIvods($term, $session_period); //下載 ivod 清單到 json/list/
+//$ivods = Downloader::getIvodList(); //透過 json/list/*.json 取得 ivod 清單
+//Downloader::downloadIvodsDetail($ivods); //下載單一 ivod 資料（with_gazette=1）到 /json/single
+$detailed_ivods = Downloader::getDetailedIvodList(); //取得含公報資訊的 ivod 資料
 $ivod_cnt = count($detailed_ivods);
 
+//將 parse 好的資料存入 term-session_period.csv 檔中
 $fp = fopen("$term-$session_period.csv", 'w');
 $headers = [
     'ivod_id',
@@ -23,9 +24,10 @@ $headers = [
     'gazette_agenda_content',
     'transcript',
 ];
+fputcsv($fp, $headers);
 fclose($fp);
 
-$batch_size = 100;
+$batch_size = 100; //批次寫入
 for ($i=0; $i < $ivod_cnt; $i = $i + $batch_size) {
     $batch_detailed_ivods = array_slice($detailed_ivods, $i, $batch_size);
     $csv_rows = Parser::parseData($batch_detailed_ivods);
